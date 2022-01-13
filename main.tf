8894ae9a56927eeeaf30de19c72ff2663b94a5cd
Nagesh Tejwani <nagesh.tejwani@gmail.com>
2022-01-13 13:12:40 +0530

    test kafka cluster

--- main.tf
+++ main.tf
@@ -1,45 +1,26 @@
-terraform {
-
-  required_version = ">=0.12"
-  
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
+resource "azurerm_resource_group" "ntpoc" {
+  name     = "ntpoc-resources"
+  location = "South India"
 }
-
-provider "azurerm" {
-  features {}
-}
-
-
-resource "azurerm_resource_group" "rg" {
-  name      = "ntpoc"
-  location  = "centralindia"
-}
-
-
 
 resource "azurerm_storage_account" "ntpoc" {
-  name                     = "nthdinsightstor"
-  resource_group_name      = "ntpoc"
-  location                 = "centralindia"
+  name                     = "hdinsightstor"
+  resource_group_name      = azurerm_resource_group.ntpoc.name
+  location                 = azurerm_resource_group.ntpoc.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "ntpoc" {
-  name                  = "nthdinsight"
+  name                  = "hdinsight"
   storage_account_name  = azurerm_storage_account.ntpoc.name
   container_access_type = "private"
 }
 
 resource "azurerm_hdinsight_kafka_cluster" "ntpoc" {
-  name                = "nthdicluster"
-  resource_group_name = "ntpoc"
-  location            = "centralindia"
+  name                = "ntpoc-hdicluster"
+  resource_group_name = azurerm_resource_group.ntpoc.name
+  location            = azurerm_resource_group.ntpoc.location
   cluster_version     = "4.0"
   tier                = "Standard"
 
@@ -49,27 +30,35 @@ resource "azurerm_hdinsight_kafka_cluster" "ntpoc" {
 
   gateway {
     enabled  = true
+    username = "acctestusrgw"
+    password = "TerrAform123!"
   }
 
   storage_account {
-    storage_container_id = nthdistorage 
-    storage_account_key  = nthdistorage.primary_access_key
+    storage_container_id = azurerm_storage_container.ntpoc.id
+    storage_account_key  = azurerm_storage_account.ntpoc.primary_access_key
     is_default           = true
   }
 
   roles {
     head_node {
       vm_size  = "Standard_D3_V2"
+      username = "acctestusrvm"
+      password = "AccTestvdSC4daf986!"
     }
 
     worker_node {
       vm_size                  = "Standard_D3_V2"
+      username                 = "acctestusrvm"
+      password                 = "AccTestvdSC4daf986!"
       number_of_disks_per_node = 3
       target_instance_count    = 3
     }
 
     zookeeper_node {
       vm_size  = "Standard_D3_V2"
+      username = "acctestusrvm"
+      password = "AccTestvdSC4daf986!"
     }
   }
 }
