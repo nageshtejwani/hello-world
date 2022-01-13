b01aebb50370925c1ed3e604db326267d5b1384b
Nagesh Tejwani <nagesh.tejwani@gmail.com>
2022-01-13 20:36:02 +0530

    Variables file

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ARM_CLIENT_ID" {
+
+    description = "Client ID of the service AKS managed service principal"
+    type =string
+  
+}
+
+
+variable "ARM_CLIENT_SECRET " {
+
+    description = "Secret of the service AKS-managed service principal"
+    type =string
+  
+}
